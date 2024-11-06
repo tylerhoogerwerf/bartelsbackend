828ccb64117eb1c78cf81140a5d09500f19381fc
Tyler Hoogerwerf <hallo@ikbentyler.nl>
2024-11-07 00:39:21 +0100

    first commit

---
+++ medusa-config.ts
@@ -0,0 +1,36 @@
+import { loadEnv, defineConfig } from '@medusajs/framework/utils'
+
+loadEnv(process.env.NODE_ENV || 'development', process.cwd())
+
+module.exports = defineConfig({
+  projectConfig: {
+    databaseUrl: process.env.DATABASE_URL,
+    http: {
+      storeCors: process.env.STORE_CORS,
+      adminCors: process.env.ADMIN_CORS,
+      authCors: process.env.AUTH_CORS,
+      jwtSecret: process.env.JWT_SECRET || "supersecret",
+      cookieSecret: process.env.COOKIE_SECRET || "supersecret",
+    },
+  },
+  modules: [
+    {
+      resolve: "@medusajs/medusa/payment",
+      options: {
+        providers: [
+          {
+            resolve: "@medusajs/medusa/payment-stripe",
+            id: "stripe",
+            options: {
+              apiKey: process.env.STRIPE_API_KEY, // Your Stripe secret key
+              webhookSecret: process.env.STRIPE_WEBHOOK_SECRET, // Optional, for deployed applications
+              capture: true, // Optional, automatic capture setting
+              automatic_payment_methods: true, // Optional, enable automatic payment methods
+              payment_description: "Bartels Tilburg", // Optional
+            },
+          },
+        ],
+      },
+    },
+  ],
+});
